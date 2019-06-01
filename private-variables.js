acfc5e181a0edf5a1d36b9e27714f72dfa624bda
rahulkannan <rahulkannan144@gmail.com>
2019-06-01 12:50:51 +0530

    Private variables in JS classes

--- private-variables.js
+++ private-variables.js
@@ -1,1 +1,15 @@
-
+class Bike {
+  #speed = 0;
+  #time = 0;
+  accelerate(speed, time) {
+    this.#speed = speed;
+    this.#time = time;
+  }
+  getDistance() {
+    return this.#speed * this.#time;
+  }
+}
+const testBike = new Bike();
+testBike.accelerate(100, 120);
+console.log(`Distance travelled ${testBike.getDistance()}`);
+console.log(testBike.speed); //Undefined
